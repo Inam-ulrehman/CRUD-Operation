c6cd8bfbb30941d0a9cf7d6e61bb10a1bb46dfda
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-05-18 17:27:36 -0400

    commit

--- src/components/SmallSideBar.js
+++ src/components/SmallSideBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { toggleSidebar } from '../features/user/userSlice'
 import ShareNav from './ShareNav'
 
